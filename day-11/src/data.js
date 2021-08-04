e06d27add49a9d1dfe68004bd0ccb537f9487db3
srilakshmi <67906452+srilakshmi199@users.noreply.github.com>
2021-08-04 18:04:09 +0530

    Add files via upload

---
+++ day-11/src/data.js
@@ -0,0 +1,142 @@
+let IndiaDetails= [
+{
+    "states":{
+        "Ap":{
+            "districts":13,
+            "villages":28_123
+            },
+        "Telangana":{
+                "districts":31,
+                "villages":12_769
+               },
+        "Arunachal Pradesh":{
+            "districts":22,
+            "villages":3_649
+           },
+        "Assam" :{
+            "districts":33,
+            "villages":11_281,
+           },
+        "Bihar":{
+            "districts":38,
+            "villages":45_103,
+           },
+        "Chandigarh":{
+            "districts":1,
+            "villages":23
+           },
+        "Chhattisgarh":{
+            "districts":27,
+            "villages":20_619
+           },
+        "Delhi (NCT)":{
+            "districts":11,
+            "villages":209
+           },
+        "Goa":{
+            "districts":2,
+            "villages":411
+           },
+
+        "Gujarat":{
+            "districts":33,
+            "villages":18_544
+           },
+        "Haryana":{
+            "districts":22,
+            "villages":6_848
+           },
+        "Himachal Pradesh":{
+            "districts":12,
+            "villages":20_690
+           },
+        "Jammu and Kashmir":{
+            "districts":14,
+            "villages":897
+           },
+        "Jharkhand":{
+            "districts":24,
+            "villages":32_620
+           },
+        "Karnataka":{
+            "districts":31,
+            "villages":29_483
+           },
+        "Kerala":{
+            "districts":14,
+            "villages":1670
+           },
+        "Lakshadweep":{
+            "districts":1,
+            "villages":10
+           },
+        "Madhya Pradesh":{
+            "districts":52,
+            "villages":51_527
+           },
+        "Maharashtra":{
+            "districts":35,
+            "villages":43_722
+           },
+        "Manipur":{
+            "districts":16,
+            "villages":1_311
+           },   
+        "Meghalaya":{
+            "districts":11,
+            "villages":6_026
+           },
+        "Mizoram":{
+            "districts":8,
+            "villages":424
+           },
+        "Nagaland":{
+            "districts":12,
+            "villages":484
+           },
+        "Odisha":{
+            "districts":30,
+            "villages":53_845
+           },
+        "Puducherry":{
+            "districts":4,
+            "villages":71
+           }, 
+           
+        "Punjab":{
+            "districts":22,
+            "villages":12_729
+           },   
+        "Rajasthan":{
+            "districts":33,
+            "villages":44_672
+           },   
+        "Sikkim":{
+            "districts":4,
+            "villages":460
+           },   
+        "Tamil Nadu":{
+            "districts":38,
+            "villages":15_979
+           }, 
+        "Tripura":{
+            "districts":8,
+            "villages":901
+           },
+        "Uttarakhand":{
+            "districts":13,
+            "villages":110
+           },
+        "Uttar Pradesh":{
+            "districts":75,
+            "villages":107_440
+           },
+        "West Bengal":{
+            "districts":23,
+            "villages":40_783
+           },
+    
+        }
+    }
+
+]
